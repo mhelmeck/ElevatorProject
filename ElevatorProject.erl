02d1e4212baee6e4c44bd70df4d79ce7e982c46b
mhelmeck <mhelmecki@gmail.com>
2018-01-24 15:06:52 +0100

    First version of animation

--- ElevatorProject.erl
+++ ElevatorProject.erl
@@ -1,6 +1,9 @@
 -module(elevatorProject).
 -compile(export_all).
 
+
+
+% - START - init_system
 % - Fl - is a Floor shortcut
 % - Elev - is a Elevator shortcut
 init_system(Fl_Amount,
@@ -15,22 +18,31 @@ init_system(Fl_Amount,
                               ->
   Raport_Manager_PID = spawn(elevatorProject, raport_manager, []),
   Draw_manager_PID = spawn(elevatorProject, draw_manager, []),
-
   put(raport_manager_PID, Raport_Manager_PID),
   put(draw_manager_PID, Draw_manager_PID),
 
+  % get(draw_manager_PID) ! {clear_board},
+  % timer:sleep(1000),
+  main_loop(5, Fl_Amount, First_Elev_Start_Fl, Second_Elev_Start_Fl).
+% - END - init_system
+
+
+
+% - START - main_loop
+main_loop(0, _, _, _) -> ok;
+main_loop(N, Fl_Amount, First_Elev_Fl, Second_Elev_Fl) ->
+  timer:sleep(1000),
   get(draw_manager_PID) ! {clear_board},
-  timer:sleep(1000),
-  get(raport_manager_PID) ! {start, First_Elev_Start_Fl, Second_Elev_Start_Fl},
-  get(draw_manager_PID) ! {draw_board, Fl_Amount, First_Elev_Start_Fl, Second_Elev_Start_Fl}.
-  % main_loop().
 
-main_loop() ->
-  io:format("TEST\n"),
-  timer:sleep(1000),
-  main_loop().
+  get(raport_manager_PID) ! {start, First_Elev_Fl, Second_Elev_Fl},
+  get(draw_manager_PID) ! {draw_board, Fl_Amount, First_Elev_Fl, Second_Elev_Fl},
+  
+  main_loop(N-1, Fl_Amount, First_Elev_Fl-1, Second_Elev_Fl+1).
+% - END - main_loop
 
-%
+
+
+% - START - raport_manager
 raport_manager() ->
   receive
     {start, A_Floor_Pos, B_Floor_Pos} ->
@@ -43,13 +55,16 @@ raport_manager() ->
       io:format("Received error message in raport_manager function\n"),
       raport_manager()
     end.
+% - END - raport_manager
 
+
+
+% - START - draw_manager
 draw_manager() ->
   receive
     {draw_board, Fl_Amount, First_Elev_Fl, Second_Elev_Fl} ->
       io:format("Drawing board:\n"),
-      % draw_board(Fl_Amount),
-      draw_board_3(Fl_Amount, First_Elev_Fl, Second_Elev_Fl),
+      draw_board(Fl_Amount, First_Elev_Fl, Second_Elev_Fl),
       draw_manager();
     {clear_board} ->
       io:format("\e[H\e[J"),
@@ -58,29 +73,27 @@ draw_manager() ->
       io:format("Received error message in draw_manager function\n"),
       draw_manager()
   end.
+% - END - draw_manager
 
-draw_board(0) -> io:format("\n\t0|  | |  | | \n");
-draw_board(X) ->
-  io:format("\n\t~p|  | |  | | ", [X]),
-  draw_board(X-1).
 
-% end_drawing
-draw_board_3(0, _, _) ->
-  io:format("\n");
+
+%
+draw_board(-1, _, _) ->
+  io:format("\n\n");
 % |X| |X|
-draw_board_3(Fl_Amount, First_Elev_Fl, Second_Elev_Fl) when First_Elev_Fl == Second_Elev_Fl
+draw_board(Fl_Amount, First_Elev_Fl, Second_Elev_Fl) when First_Elev_Fl == Second_Elev_Fl
                                                     andalso First_Elev_Fl == Fl_Amount ->
   io:format("\n\t~p|  |X|  |X| ", [Fl_Amount]),
-  draw_board_3(Fl_Amount-1, First_Elev_Fl, Second_Elev_Fl);
+  draw_board(Fl_Amount-1, First_Elev_Fl, Second_Elev_Fl);
 % |X| | |
-draw_board_3(Fl_Amount, First_Elev_Fl, Second_Elev_Fl) when First_Elev_Fl == Fl_Amount ->
+draw_board(Fl_Amount, First_Elev_Fl, Second_Elev_Fl) when First_Elev_Fl == Fl_Amount ->
   io:format("\n\t~p|  |X|  | | ", [Fl_Amount]),
-  draw_board_3(Fl_Amount-1, First_Elev_Fl, Second_Elev_Fl);
+  draw_board(Fl_Amount-1, First_Elev_Fl, Second_Elev_Fl);
 % | | |X|
-draw_board_3(Fl_Amount, First_Elev_Fl, Second_Elev_Fl) when Second_Elev_Fl == Fl_Amount ->
+draw_board(Fl_Amount, First_Elev_Fl, Second_Elev_Fl) when Second_Elev_Fl == Fl_Amount ->
   io:format("\n\t~p|  | |  |X| ", [Fl_Amount]),
-  draw_board_3(Fl_Amount-1, First_Elev_Fl, Second_Elev_Fl);
+  draw_board(Fl_Amount-1, First_Elev_Fl, Second_Elev_Fl);
 % | | | |
-draw_board_3(Fl_Amount, First_Elev_Fl, Second_Elev_Fl) ->
+draw_board(Fl_Amount, First_Elev_Fl, Second_Elev_Fl) ->
   io:format("\n\t~p|  | |  | | ", [Fl_Amount]),
-  draw_board_3(Fl_Amount-1, First_Elev_Fl, Second_Elev_Fl).
+  draw_board(Fl_Amount-1, First_Elev_Fl, Second_Elev_Fl).
