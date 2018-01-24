bfad745d80342b04e3f75e914289e007c84fdb63
mhelmeck <mhelmecki@gmail.com>
2018-01-24 14:12:59 +0100

    Added draw_manager in the simplest version

--- ElevatorProject.erl
+++ ElevatorProject.erl
@@ -14,10 +14,16 @@ init_system(Fl_Amount,
                               andalso Second_Elev_Start_Fl =< Fl_Amount
                               ->
   Raport_Manager_PID = spawn(elevatorProject, raport_manager, []),
-  put(raport_manager_PID, Raport_Manager_PID),
+  Draw_manager_PID = spawn(elevatorProject, draw_manager, []),
 
+  put(raport_manager_PID, Raport_Manager_PID),
+  put(draw_manager_PID, Draw_manager_PID),
+
+  get(draw_manager_PID) ! {clear_board},
+  timer:sleep(1000),
   get(raport_manager_PID) ! {start, First_Elev_Start_Fl, Second_Elev_Start_Fl},
-  main_loop().
+  get(draw_manager_PID) ! {draw_board, Fl_Amount}.
+  % main_loop().
 
 main_loop() ->
   io:format("TEST\n"),
@@ -38,4 +44,29 @@ raport_manager() ->
       raport_manager()
     end.
 
-% draw_manager() ->
+draw_manager() ->
+  receive
+    {draw_board, Fl_Amount} ->
+      % Fl_List = lists:seq(0,Fl_Amount),
+      io:format("Drawing board:\n"),
+      % draw_board(Fl_List),
+      draw_board_2(Fl_Amount),
+      draw_manager();
+    {clear_board} ->
+      io:format("\e[H\e[J"),
+      draw_manager();
+    _ ->
+      io:format("Received error message in draw_manager function\n"),
+      draw_manager()
+  end.
+
+draw_board([]) -> io:format("\n");
+draw_board([H|T]) ->
+  io:format("\n\t~p", [H]),
+  draw_board(T).
+
+
+draw_board_2(0) -> io:format("\n\t0|  | |  | | \n");
+draw_board_2(X) ->
+  io:format("\n\t~p|  | |  | | ", [X]),
+  draw_board_2(X-1).
